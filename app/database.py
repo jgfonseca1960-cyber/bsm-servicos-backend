baded687c7460926f3a094a7ae2382ab8e6d80d1
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-09 09:10:09 -0300

    fix metadata duplicate final

--- app/database.py
+++ app/database.py
@@ -1,16 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 from app.core.config import settings
 
-import os
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-
-# DATABASE_URL = "postgresql+psycopg://postgres:brg7573x@localhost/bsm_servicos"
 
 engine = create_engine(settings.DATABASE_URL)
 
@@ -19,7 +10,6 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
-
 
 Base = declarative_base()
 
--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
 from fastapi import FastAPI
+
 from app.database import engine, Base
 
-from app.models import usuario_model
-from app.models import empresa_model
-from app.models import avaliacao_model
+import app.models.usuario_model
+import app.models.empresa_model
+import app.models.avaliacao_model
 
 from app.routers import auth, empresa, avaliacao
 
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Rotas
+
+Base.metadata.create_all(bind=engine)
+
+
 app.include_router(auth.router)
 app.include_router(empresa.router)
 app.include_router(avaliacao.router)
