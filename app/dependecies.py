fbf70a79472eafe1f577ebb0db9a7180120f9295
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-09 12:02:09 -0300

    fix auth import path

--- app/dependecies.py
+++ app/dependecies.py
@@ -2,6 +2,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
+from app.routers.auth import get_current_user
 
 from app.database import SessionLocal
 from app.models import Usuario
