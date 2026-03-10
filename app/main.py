593e6b2613458c9ef9652a695d12e69eb25fea96
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-10 10:05:56 -0300

    remove drop_all

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 @app.on_event("startup")
 def startup():
-    Base.metadata.drop_all(bind=engine)
+    # Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
 
