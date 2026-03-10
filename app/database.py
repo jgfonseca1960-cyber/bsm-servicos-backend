74a2c8122ff5f3bd8a1f00c7e51f7ecf13ac7f59
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-10 19:47:52 -0300

    reset table empresas

--- app/database.py
+++ app/database.py
@@ -2,6 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
+from app.database import engine
+from app.models import empresa_model
+
+empresa_model.Base.metadata.drop_all(bind=engine)
+empresa_model.Base.metadata.create_all(bind=engine)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
