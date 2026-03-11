f4948ea1d0580aeeaf2e2708c9a4f16e4c4c426d
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-11 09:31:33 -0300

    add categoria column

--- app/main.py
+++ app/main.py
@@ -9,6 +9,12 @@ app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+with engine.connect() as conn:
+    conn.execute(
+        "ALTER TABLE empresas ADD COLUMN categoria VARCHAR;"
+    )
+    conn.commit()
+    
 app.include_router(auth.router)
 app.include_router(empresa.router)
 app.include_router(avaliacao.router)
