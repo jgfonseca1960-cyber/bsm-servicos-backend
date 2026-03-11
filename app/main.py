3a7c2f75294c11594386033bb76efd512a501f3c
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-10 23:46:59 -0300

    update tables create_all

--- app/main.py
+++ app/main.py
@@ -1,28 +1,14 @@
 from fastapi import FastAPI
+
 from app.database import engine, Base
-from app.models import empresa_model
-from app.models import usuario_model
-from app import models
+from app.models import empresa_model, usuario_model, avaliacao_model
+
 from app.routers import auth, empresa, avaliacao
-
-import app.models.usuario_model
-import app.models.empresa_model
-import app.models.avaliacao_model
-
 
 app = FastAPI()
 
-
-@app.on_event("startup")
-def startup():
-    # Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-
+Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(empresa.router)
 app.include_router(avaliacao.router)
-
-# @app.get("/")
-# def root():
-#    return {"status": "API funcionando"}
