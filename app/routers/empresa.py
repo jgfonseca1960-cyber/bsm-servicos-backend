f1e1a2ce1bf021f52b98ac91bc7aa988dca5e8e6
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-11 12:59:21 -0300

    fix router empresa

--- app/routers/empresa.py
+++ app/routers/empresa.py
@@ -1,3 +1,29 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+
+from app.database import SessionLocal
+from app.models.empresa_model import Empresa
+from app.models.usuario_model import Usuario
+from app.models.avaliacao_model import Avaliacao
+
+from app.schemas.empresa_schema import EmpresaCreate
+from ..dependencies import get_current_user
+
+
+# ✅ router precisa vir antes dos endpoints
+router = APIRouter(prefix="/empresas", tags=["Empresas"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# ✅ CRIAR EMPRESA
 @router.post("/")
 def criar_empresa(
     empresa: EmpresaCreate,
@@ -17,3 +43,20 @@ def criar_empresa(
     db.refresh(nova_empresa)
 
     return nova_empresa
+
+
+# ✅ RANKING
+@router.get("/ranking")
+def ranking(db: Session = Depends(get_db)):
+
+    res = db.query(
+        Empresa.nome,
+        func.avg(Avaliacao.nota).label("media"),
+        func.count(Avaliacao.id).label("total")
+    ).join(
+        Avaliacao,
+        Avaliacao.empresa_id == Empresa.id,
+        isouter=True
+    ).group_by(Empresa.id).all()
+
+    return res
