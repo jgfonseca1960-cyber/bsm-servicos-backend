24b7827e2d841870345775b52ae391c97af2d6ab
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-12 18:37:18 -0300

    add usuario_id empresa

--- app/models/empresa_model.py
+++ app/models/empresa_model.py
@@ -1,41 +1,31 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-
 from app.database import Base
-# from app.db.base import Base
 
 
 class Empresa(Base):
-
     __tablename__ = "empresas"
 
     id = Column(Integer, primary_key=True, index=True)
 
-    nome = Column(String)
-
+    nome = Column(String, nullable=False)
     responsavel = Column(String)
-
     telefone = Column(String)
-
     endereco = Column(String)
 
-    bairro = Column(String(100))
-
     cidade = Column(String)
-
     estado = Column(String)
+    bairro = Column(String)
 
     tipo_servico = Column(String)
+    categoria = Column(String)
 
     descricao = Column(String)
 
-    categoria = Column(String)
-
     latitude = Column(Float)
-
     longitude = Column(Float)
 
-    usuarios = relationship(
-        "Usuario",
-        back_populates="empresa"
-    )
+    # ✅ ADICIONAR ISSO
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"))
+
+    usuario = relationship("Usuario")
