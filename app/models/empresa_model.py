9ea7ee6fcafde6f9a139f9ee6b51d6a68794a9e2
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-13 11:27:47 -0300

    fix relationship error

--- app/models/empresa_model.py
+++ app/models/empresa_model.py
@@ -1,33 +1,17 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float
+
 from app.database import Base
 
 
 class Empresa(Base):
     __tablename__ = "empresas"
-
     id = Column(Integer, primary_key=True, index=True)
-
     nome = Column(String, nullable=False)
     cidade = Column(String, nullable=False)
-
     tipo_servico = Column(String)
     categoria = Column(String)
-
     telefone = Column(String)
     endereco = Column(String)
     descricao = Column(String)
-
     latitude = Column(Float)
-    longitude = Column(Float)
-
-    usuario_id = Column(
-        Integer,
-        ForeignKey("usuarios.id")
-    )
-
-    usuario = relationship(
-        "Usuario",
-        back_populates="empresas",
-        foreign_keys=[usuario_id]
-    )
+    longitude = Column(Float)    
--- app/models/usuario_model.py
+++ app/models/usuario_model.py
@@ -3,13 +3,9 @@ from app.database import Base
 
 
 class Usuario(Base):
-
     __tablename__ = "usuarios"
 
     id = Column(Integer, primary_key=True, index=True)
-
-    nome = Column(String)
-    email = Column(String, unique=True, index=True)
-    senha = Column(String)
-
-    tipo = Column(String)
+    nome = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    senha = Column(String, nullable=False)
