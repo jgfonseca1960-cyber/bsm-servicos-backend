beca69079cfcb6450e5b6732639121d104e07275
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-15 19:23:02 -0300

    fix cors render

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
 
@@ -7,11 +8,23 @@ from app.routers import usuario
 from app.routers import empresa
 from app.routers import avaliacao
 
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="BSM API"
 )
+
+
+# ✅ CORS LIBERADO
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(
     auth.router,
