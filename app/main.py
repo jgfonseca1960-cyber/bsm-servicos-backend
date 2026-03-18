82e5dfb09b0cb3d579e6faaf4191d5b69fdb1af2
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-17 21:38:53 -0300

    fix create_all models import

--- app/main.py
+++ app/main.py
@@ -2,17 +2,23 @@ from fastapi import FastAPI
 
 from app.database import Base, engine
 
+# ✅ IMPORTAR MODELS (OBRIGATÓRIO)
+from app.models.usuario_model import Usuario
+from app.models.empresa_model import Empresa
+
 from app.routers import auth
 from app.routers import usuario
 from app.routers import empresa
 
 
+# ✅ RESET DO BANCO
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
     title="BSM Serviços API"
 )
-
 
 app.include_router(auth.router)
 app.include_router(usuario.router)
