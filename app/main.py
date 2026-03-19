9bfa9779e86a00b8c85524e71e8bcb08143f84eb
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-19 20:08:39 -0300

    reset tabela empresas

--- app/main.py
+++ app/main.py
@@ -6,11 +6,6 @@ from app.database import Base, engine
 from app.models.usuario_model import Usuario
 from app.models.empresa_model import Empresa
 
-
-Empresa.__table__.drop(bind=engine, checkfirst=True)
-Base.metadata.create_all(bind=engine)
-
-
 # IMPORTAR ROUTERS
 from app.routers import auth
 from app.routers import usuario
@@ -18,10 +13,12 @@ from app.routers import empresa
 
 app = FastAPI()
 
+# Temporario para resetar tabela 
+
+Empresa.__table__.drop(bind=engine, checkfirst=True)
 
 # criar tabelas
 Base.metadata.create_all(bind=engine)
-
 
 # rotas
 app.include_router(auth.router)
