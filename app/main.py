e7fbc41c60c3845c173946ff9ae5ffad176b4468
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-20 11:32:10 -0300

    remove reset db

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from sqlalchemy import text
 
 from app.database import Base, engine
 
@@ -11,14 +10,6 @@ from app.routers import usuario
 from app.routers import empresa
 
 app = FastAPI()
-
-
-# RESET BANCO (TEMPORÁRIO)
-with engine.connect() as conn:
-    conn.execute(text("DROP TABLE IF EXISTS avaliacoes CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS empresas CASCADE"))
-    conn.execute(text("DROP TABLE IF EXISTS usuarios CASCADE"))
-    conn.commit()
 
 
 Base.metadata.create_all(bind=engine)
