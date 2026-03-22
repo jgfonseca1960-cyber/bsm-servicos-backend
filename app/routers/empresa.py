7d6bbf4084bc3565b17d275316462dec389e7a05
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-22 17:54:42 -0300

    Busca por categoria ou nome

--- app/routers/empresa.py
+++ app/routers/empresa.py
@@ -173,3 +173,71 @@ def lista_completa():
         """))
 
         return [dict(r._mapping) for r in result]
+    
+    ## endpoint pesquisa por nome
+
+    from fastapi import Query
+
+    @router.get("/busca")
+def busca(
+    categoria_id: int | None = None,
+    nome: str | None = None,
+    cidade: str | None = None,
+):
+
+    sql = """
+
+        SELECT
+
+            e.id,
+            e.nome,
+            e.cidade,
+            e.bairro,
+            e.logo,
+
+            c.nome as categoria,
+
+            COALESCE(AVG(a.nota),0) as media,
+            COUNT(a.id) as total_avaliacoes
+
+        FROM empresas e
+
+        LEFT JOIN categorias c
+        ON c.id = e.categoria_id
+
+        LEFT JOIN avaliacoes a
+        ON a.empresa_id = e.id
+
+        WHERE 1=1
+
+    """
+
+    params = {}
+
+    if categoria_id:
+        sql += " AND e.categoria_id = :categoria_id"
+        params["categoria_id"] = categoria_id
+
+    if nome:
+        sql += " AND LOWER(e.nome) LIKE LOWER(:nome)"
+        params["nome"] = f"%{nome}%"
+
+    if cidade:
+        sql += " AND LOWER(e.cidade) LIKE LOWER(:cidade)"
+        params["cidade"] = f"%{cidade}%"
+
+    sql += """
+
+        GROUP BY
+            e.id,
+            c.nome
+
+        ORDER BY e.nome
+
+    """
+
+    with engine.connect() as conn:
+
+        result = conn.execute(text(sql), params)
+
+        return [dict(r._mapping) for r in result]
