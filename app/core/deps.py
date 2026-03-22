80efc6e3c1fe0fc33b1a9bfe8f43c6bdf53ab8ab
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-22 19:08:44 -0300

    Ajustes Token

--- app/core/deps.py
+++ app/core/deps.py
@@ -1,53 +1,26 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt
-import os
-from sqlalchemy.orm import Session
 
-from app.database import get_db
-from app.models.usuario_model import Usuario
+# ✅ cria o esquema do token para Swagger
 
-oauth2 = OAuth2PasswordBearer(
+oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="/auth/login"
 )
 
-SECRET = os.getenv("SECRET_KEY", "123")
 
+# ✅ função que pega usuário do token
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(
+    token: str = Depends(oauth2_scheme)
+):
 
     if not token:
+
         raise HTTPException(
             status_code=401,
             detail="Token inválido"
         )
 
+    # depois vamos validar JWT de verdade
     return {"id": 1}
-
-    try:
-
-        payload = jwt.decode(
-            token,
-            SECRET,
-            algorithms=["HS256"]
-        )
-
-        user_id = int(payload["sub"])
-
-        user = db.query(Usuario).filter(
-            Usuario.id == user_id
-        ).first()
-
-        if not user:
-            raise HTTPException(
-                status_code=401,
-                detail="Usuario não encontrado"
-            )
-
-        return user
-
-    except:
-        raise HTTPException(
-            status_code=401,
-            detail="Token inválido"
-        )
