5e0fd0ea6ef88fa19d8037dcd1204d6df2607f1a
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-24 20:10:35 -0300

    Ajustes Cloud

--- app/core/cloudinary_config.py
+++ app/core/cloudinary_config.py
@@ -3,6 +3,5 @@ import cloudinary
 cloudinary.config(
     cloud_name="dksia9lvn",
     api_key="891618874311661",
-    api_secret="4pblSg9nJDhwavz9PMeHhnalcxM"
-UA_API_SECRET"
+    api_secret="4pblSg9nJDhwavz9PMeHhnalcxM
 )
