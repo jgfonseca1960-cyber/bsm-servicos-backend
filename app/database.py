682dfec37a3ba9fbcaa7838c371f35ca02ebf5be
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-27 19:53:41 -0300

    ajustes finais

--- app/database.py
+++ app/database.py
@@ -1,28 +1,11 @@
-# backend/app/database.py
-
-import sys
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy.orm import Session
-from app.config import DATABASE_URL
-from app.database import base, engine
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-# 🔹 Ajuste do path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
-
 from app.config import DATABASE_URL
 
 # Base dos modelos
 Base = declarative_base()
 
+# Engine
 engine = create_engine(
     DATABASE_URL,
     echo=True,
@@ -39,17 +22,20 @@ SessionLocal = sessionmaker(
     future=True
 )
 
+# Dependência do FastAPI
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Inicialização do banco
 def init_db():
     print("🔥 Criando todas as tabelas...")
 
-    # 🔥 Importação LOCAL (evita circular import)
-    import app.models  # noqa: F401
+    import app.models  # importa models
 
     Base.metadata.create_all(bind=engine)
 
     print("✅ Todas as tabelas foram criadas!")
-
-
-# Execução direta
-if __name__ == "__main__":
-    init_db()
