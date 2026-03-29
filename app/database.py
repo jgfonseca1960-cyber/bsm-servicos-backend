2d701e0e5517aa2d914a7090b4efa3746a76ca08
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-29 10:21:37 -0300

    Ajustar 9

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
@@ -13,13 +13,20 @@ Base = declarative_base()
 def init_db():
     print("Recriando banco...")
 
+    # IMPORTAR MODELOS
     from app.models.usuario_model import Usuario
     from app.models.empresa_model import Empresa
     from app.models.empresa_foto_model import EmpresaFoto
     from app.models.servico_model import Servico
     from app.models.tipo_servico_model import TipoServico
 
-    Base.metadata.drop_all(bind=engine)   # (pode deixar por enquanto)
+    # 💥 RESET TOTAL DO BANCO (resolve erro de dependência)
+    with engine.connect() as conn:
+        conn.execute(text("DROP SCHEMA public CASCADE"))
+        conn.execute(text("CREATE SCHEMA public"))
+        conn.commit()
+
+    # 🔥 recria todas as tabelas
     Base.metadata.create_all(bind=engine)
 
-    print("Banco recriado!")
+    print("Banco recriado do ZERO!")
