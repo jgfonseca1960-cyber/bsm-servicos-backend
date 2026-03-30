584c4074b4a749bfc3b5a07eb240d5340e693a72
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-30 10:03:38 -0300

    Ajustes Finais

--- app/database.py
+++ app/database.py
@@ -9,7 +9,10 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
