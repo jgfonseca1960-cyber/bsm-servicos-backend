46a0957dc45e50bb0b476f8617710e6fcda0ef13
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-04-01 11:01:57 -0300

    Ajuste Token 11

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -3,11 +3,18 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from app.core.security import verificar_token
 
-security = HTTPBearer()
+security = HTTPBearer(auto_error=False)
 
 
 def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if not credentials:
+        raise HTTPException(status_code=401, detail="Token não informado")
+
     token = credentials.credentials
+
+    # 🔥 REMOVE "Bearer " AUTOMATICAMENTE SE VIER JUNTO
+    if token.startswith("Bearer "):
+        token = token.replace("Bearer ", "")
 
     payload = verificar_token(token)
 
