d6a29fd8923a78c8df97cef6e456dcf33ccaa373
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-03-31 22:11:06 -0300

    Token 10

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,23 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-from passlib.context import CryptContext
-
-from app.database import get_db
-from app.models.usuario_model import Usuario
-from pydantic import BaseModel
-
-# 🔥 JWT
 from app.core.security import criar_token
-
-router = APIRouter(prefix="/auth", tags=["Auth"])
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-class LoginRequest(BaseModel):
-    email: str
-    senha: str
-
 
 @router.post("/login")
 def login(data: LoginRequest, db: Session = Depends(get_db)):
@@ -29,7 +10,6 @@ def login(data: LoginRequest, db: Session = Depends(get_db)):
     if not pwd_context.verify(data.senha, usuario.senha_hash):
         raise HTTPException(status_code=401, detail="Senha inválida")
 
-    # 🔥 AQUI ESTÁ O JWT (ANTES NÃO TINHA)
     token = criar_token({
         "sub": str(usuario.id),
         "is_admin": usuario.is_admin
--- app/core/security.py
+++ app/core/security.py
@@ -1,23 +1,25 @@
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
 
-from app.core.config import SECRET_KEY, ALGORITHM
+from app.core.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
+
+# 🔐 GERAR TOKEN
 def criar_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(minutes=60)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
 
-    return encoded_jwt
 
-
+# 🔍 VALIDAR TOKEN
 def verificar_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
-
-    except JWTError:
+    except JWTError as e:
+        print("ERRO TOKEN:", str(e))  # 👈 IMPORTANTE PRA DEBUG
         return None
