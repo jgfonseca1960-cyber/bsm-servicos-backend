1b54fd2a97f98083f40bafe6447eb9a7da9e7dca
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-04-08 15:39:26 -0300

    Ajustes 33

--- app/schemas/servico_schema.py
+++ app/schemas/servico_schema.py
@@ -1,34 +1,16 @@
 from pydantic import BaseModel
-from typing import Optional
 
 
-# 🔹 Base
 class ServicoBase(BaseModel):
     nome: str
 
 
-# 🔹 Criação
 class ServicoCreate(ServicoBase):
     pass
 
 
-# 🔹 Atualização
-class ServicoUpdate(BaseModel):
-    nome: Optional[str] = None
-
-
-# 🔹 Resposta completa
 class ServicoResponse(ServicoBase):
     id: int
-
-    class Config:
-        from_attributes = True
-
-
-# 🔹 Versão simples (para usar dentro de Empresa)
-class ServicoSimple(BaseModel):
-    id: int
-    nome: str
 
     class Config:
         from_attributes = True
--- app/services/servico_service.py
+++ app/services/servico_service.py
@@ -1,31 +1,34 @@
-from sqlalchemy.orm import Session
 from app.models.servico_model import Servico
-from app.models.empresa_model import Empresa
-from app.models.empresa_foto_model import EmpresaFoto
-from app.models.usuario_model import Usuario
-from app.repositories import servico_repo
+from app.schemas.servico_schema import ServicoCreate
 
-def criar_servico(db: Session, data):
-    servico = Servico(
-        empresa_id=data.empresa_id,
-        nome=data.nome,
-        descricao=data.descricao
+
+def criar_servico(db: Session, data: ServicoCreate):
+    novo_servico = Servico(
+        nome=data.nome
     )
-    return servico_repo.create_servico(db, servico)
 
-def listar_servico(db: Session):
-    return servico_repo.get_servico(db)
+    db.add(novo_servico)
+    db.commit()
+    db.refresh(novo_servico)
 
-def listar_servico_por_empresa(db: Session, empresa_id: int):
-    return servico_repo.get_servico_por_empresa(db, empresa_id)
+    return novo_servico
 
-def atualizar_servico(db: Session, servico_id: int, data):
-    servico = servico_repo.get_servico(db, servico_id)
-    if not servico:
-        return None
-    servico.nome = data.nome
-    servico.descricao = data.descricao
-    return servico_repo.update_servico(db, servico)
+
+def listar_servicos(db: Session):
+    return db.query(Servico).all()
+
+
+def get_servico_por_id(db: Session, servico_id: int):
+    return db.query(Servico).filter(Servico.id == servico_id).first()
+
 
 def deletar_servico(db: Session, servico_id: int):
-    return servico_repo.delete_servico(db, servico_id)
+    servico = get_servico_por_id(db, servico_id)
+
+    if not servico:
+        return None
+
+    db.delete(servico)
+    db.commit()
+
+    return servico
