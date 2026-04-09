d46b36bdad659ca042f4ad28a5d458498ab3c388
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-04-09 12:59:47 -0300

    Ajustes Tabela 1

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
-from sqlalchemy import text
-from fastapi import Depends
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 from app.database import get_db
 
+# 👇 PRIMEIRO cria o app
+app = FastAPI()
+
+# 👇 DEPOIS as rotas
 @app.get("/atualizar-banco")
 def atualizar_banco(db: Session = Depends(get_db)):
 
-    # adiciona coluna servico_id
     db.execute(text("""
         ALTER TABLE empresas 
         ADD COLUMN IF NOT EXISTS servico_id INTEGER
     """))
 
-    # cria tabela de fotos
     db.execute(text("""
         CREATE TABLE IF NOT EXISTS empresa_fotos (
             id SERIAL PRIMARY KEY,
