4b3176f43bd36f869dedd4c744bbec8ece2eae43
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-04-09 20:09:48 -0300

    Ajuste Empresa  Xy

--- app/controllers/empresa_controller.py
+++ app/controllers/empresa_controller.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from app.database import get_db
-from app.models.empresa import Empresa
+from app.models.empresa_model import Empresa
 
 # 🔹 SERVICE
 from app.services.empresa_service import (
