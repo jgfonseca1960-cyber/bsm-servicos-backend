213e6e7062b9e21404ab800ee56a168c630747a7
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-04-09 08:50:25 -0300

    Ajustes 02

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,4 +2,3 @@ from app.models.empresa_model import Empresa
 from app.models.servico_model import Servico
 from app.models.empresa_foto_model import EmpresaFoto
 from app.models.usuario_model import Usuario
-from app.models.foto_model import Foto
