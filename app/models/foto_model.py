2952ecdb71d93afb8040336788ce87efc6dc7183
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-04-09 08:32:00 -0300

    AJustes 01

--- app/models/foto_model.py
+++ app/models/foto_model.py
@@ -2,8 +2,8 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
 
-class Foto(Base):
-    __tablename__ = "fotos"
+class EmpresaFoto(Base):
+    __tablename__ = "empresa_fotos"
 
     id = Column(Integer, primary_key=True, index=True)
     url = Column(String, nullable=False)
