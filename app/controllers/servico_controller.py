f392a03d0befab471b7105a8893b8f37a718a667
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-04-14 09:29:57 -0300

    Ajuste em Serviço

--- app/controllers/servico_controller.py
+++ app/controllers/servico_controller.py
@@ -4,7 +4,8 @@ from app.database import get_db
 from app.services import servico_service
 from app.schemas import ServicoCreate, ServicoResponse
 
-router = APIRouter(prefix="/servicos", tags=["Serviços"])
+# 🔥 SEM prefix aqui (já vem do main.py)
+router = APIRouter(tags=["Serviços"])
 
 
 @router.post("/", response_model=ServicoResponse)
@@ -13,10 +14,5 @@ def criar_servico(servico: ServicoCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/", response_model=list[ServicoResponse])
-def listar_servico(db: Session = Depends(get_db)):
-    return servico_service.listar_servicos(db)
-
-@router.get("/servico")
 def listar_servicos(db: Session = Depends(get_db)):
-    return db.query(Servico).all()
-
+    return servico_service.listar_servicos(db)
