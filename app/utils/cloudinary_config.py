2ce5474f162292b2bfdca5fb4c161e039c2c176a
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-04-23 18:12:05 -0300

    Ajustes fotos 010

--- app/utils/cloudinary_config.py
+++ app/utils/cloudinary_config.py
@@ -4,6 +4,4 @@ import os
 cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-    secure=True
-)
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),)
