7a2a87c222ef187a94820478cbfd78ac5e3eb033
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-04-23 18:05:29 -0300

    Ajustes fotos 010

--- app/core/cloudinary_config.py
+++ app/core/cloudinary_config.py
@@ -1,8 +1,7 @@
 import cloudinary
-
+import os
 
 cloudinary.config(
-    cloud_name="dksia9lvn",
-    api_key="891618874311661",
-    api_secret="4pblSg9nJDhwavz9PMeHhnalcxM"
-)
+    cloud_name=os.getenv("dksia9lvn"),
+    api_key=os.getenv("891618874311661"),
+    api_secret=os.getenv("4pblSg9nJDhwavz9PMeHhnalcxM"))
