3b0948dd9c24506d8caeea1ceb3f793f2f4fb34f
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-05-08 18:11:33 -0300

    fix database url

--- app/database.py
+++ app/database.py
@@ -1,39 +1,32 @@
+import os
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# 🔥 URL fixa (temporário pra garantir que funciona)
-DATABASE_URL = "postgresql+psycopg2://bsm_user:CFFwJtRUdOODyLDWy4mwY8ZDX4iJ77iS@dpg-d7b6jjkvjg8s73etqljg-a.oregon-postgres.render.com/bsm_servicos_4ccv?sslmode=require"
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    pool_pre_ping=True,
 )
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
 
 Base = declarative_base()
 
-# 🔥 FUNÇÃO QUE O MAIN PRECISA
-def init_db():
-    from app.models import (
-        empresa_model,
-        empresa_foto_model,
-        usuario_model,
-        servico_model,
-    )
-    Base.metadata.create_all(bind=engine)
 
-    # =========================
-# 🔥 DEPENDÊNCIA DO FASTAPI
-# =========================
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
