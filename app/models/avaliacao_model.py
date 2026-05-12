fa9db3854d469b02f1ca56c8bb9a1bc0b0600363
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-05-12 17:23:11 -0300

    fix: remove markdown do schema

--- app/models/avaliacao_model.py
+++ app/models/avaliacao_model.py
@@ -6,7 +6,7 @@ class Avaliacao(Base):
     __tablename__ = "avaliacoes"
 
     id = Column(Integer, primary_key=True, index=True)
-    empresa_id = Column(Integer, ForeignKey("empresa.id"))
+    empresa_id = Column(Integer, ForeignKey("empresas.id"))
     usuario_id = Column(Integer, ForeignKey("usuarios.id"))
     nota = Column(Integer)
     comentario = Column(String)
