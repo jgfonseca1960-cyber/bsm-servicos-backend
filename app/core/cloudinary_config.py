90b76913d0c3a75a26647efb0f7192bdeec323d1
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-06-03 17:52:29 -0300

    Acerta Acesso BackEnd 1

--- app/core/cloudinary_config.py
+++ app/core/cloudinary_config.py
@@ -4,4 +4,7 @@ import os
 cloudinary.config(
     cloud_name=os.getenv("dksia9lvn"),
     api_key=os.getenv("477495685379182"),
-    api_secret=os.getenv("HBNvXFfrl8fa7s3a2v5VjSPftz0"))
+    api_secret=os.getenv("HBNvXFfrl8fa7s3a2v5VjSPftz0"),
+    secure=True)
+
+print("🔥 Cloudinary carregado!")
