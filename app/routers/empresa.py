58f2b316c5ed8e7143ea50020392ba54487aa9b2
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-06-03 19:19:14 -0300

    Acerta Cloudinary 777

--- app/routers/empresa.py
+++ app/routers/empresa.py
@@ -65,14 +65,14 @@ router = APIRouter(
 # ☁️ CLOUDINARY
 # =========================================================
 
-cloudinary.config(
-    cloud_name=os.getenv("dksia9lvn"),
-    api_key=os.getenv("477495685379182"),
-    api_secret=os.getenv("HBNvXFfrl8fa7s3a2v5VjSPftz0"),
-    secure=True
-)
+# cloudinary.config(
+#    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+#    api_key=os.getenv("CLOUDINARY_API_KEY"),
+#    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+#    secure=True
+#)
 
-print("🔥 Cloudinary carregado!")
+#print("🔥 Cloudinary carregado!")
 
 # =========================================================
 # 📷 FOTO PRINCIPAL
