4f29b4acaa2107c1f14cf62f5e4d4699a0aa660a
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-06-03 10:40:27 -0300

    Acerta Acesso Fotos Cloudi

--- app/config/cloudinary_config.py
+++ app/config/cloudinary_config.py
@@ -2,8 +2,8 @@ import cloudinary
 import os
 
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),)
+    cloud_name=os.getenv("dksia9lvn"),
+    api_key=os.getenv("477495685379182"),
+    api_secret=os.getenv("HBNvXFfrl8fa7s3a2v5VjSPftz0"),)
 
 print("🔥 Cloudinary carregado!")
--- app/core/cloudinary_config.py
+++ app/core/cloudinary_config.py
@@ -3,5 +3,5 @@ import os
 
 cloudinary.config(
     cloud_name=os.getenv("dksia9lvn"),
-    api_key=os.getenv("891618874311661"),
-    api_secret=os.getenv("4pblSg9nJDhwavz9PMeHhnalcxM"))
+    api_key=os.getenv("477495685379182"),
+    api_secret=os.getenv("HBNvXFfrl8fa7s3a2v5VjSPftz0"))
