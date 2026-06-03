84c9e4f307ccd5a4d7432d3a76546baa687f90b8
jgfonseca1960-cyber <jgfonseca1960@gmail.com>
2026-06-03 09:31:25 -0300

    Acerta Acesso Fotos

--- app/routers/empresa.py
+++ app/routers/empresa.py
@@ -66,9 +66,9 @@ router = APIRouter(
 # =========================================================
 
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    cloud_name=os.getenv("dksia9lvn"),
+    api_key=os.getenv("477495685379182"),
+    api_secret=os.getenv("HBNvXFfrl8fa7s3a2v5VjSPftz0"),
     secure=True
 )
 
